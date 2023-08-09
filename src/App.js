d0edb13a1208afb1cbb7ac802ff747934cc5fbc0
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-08-09 20:24:57 +0200

    Even more CSS changes, added some text to the header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
+      <div className="dictionary-phonetics">/ˈdɪkʃəˌnɛɹi/</div>
+      <div className="dictionary-meaning">
+        "a book or electronic resource that lists the words of a language and
+        gives their meaning "
+      </div>
+      <div className="header-enter-text">
+        what word would you like to look up?
+      </div>
       <main>
         <Dictionary />
       </main>
-      <footer>Coded by Steph (that's me!)</footer>
+      <footer>
+        <div>
+          <h3>credits</h3>
+          <div className="footer-text">Coded by Steph (that's me!)</div>
+        </div>
+      </footer>
     </div>
   );
 }
