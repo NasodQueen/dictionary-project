fc1538b5ada0687fef0535421b952a616c84a902
NasodQueen <119695089+NasodQueen@users.noreply.github.com>
2023-08-11 18:28:39 +0200

    Implemented audio playback on button press

--- src/PhoneticAudio.js
+++ src/PhoneticAudio.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { IconVolume, IconVolumeOff } from "@tabler/icons-react";
+import useSound from "use-sound";
 
 import "./Dictionary.css";
 
@@ -8,10 +9,17 @@ import "./Dictionary.css";
 // https://www.npmjs.com/package/react-audio-player
 
 export default function PhoneticAudio(props) {
+  let soundUrl = props.phonetics.audio;
+  const [play] = useSound(soundUrl, { volume: 0.25 });
+
+  function handledClick() {
+    play();
+  }
+
   if (props.phonetics.audio) {
     return (
       <div>
-        <button className="sound-button">
+        <button className="sound-button" onClick={handledClick}>
           {/* <a href={props.phonetics.audio} rel="noreferrer" target="_blank"> */}
           <IconVolume size={24} />
           {/* </a> */}
